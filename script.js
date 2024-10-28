2db513d13804fc10bbb7faaeb888f65b0e209a8f
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-10-28 20:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const removeButton = document.querySelector('input[type="button"]');
+const colorSelect = document.getElementById('colorSelect');
+
+const removeSelectedColor = () => {
+    const selectedIndex = colorSelect.selectedIndex;
+    
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
+};
+
+// Add event listener to the button
+removeButton.addEventListener('click', removeSelectedColor);
